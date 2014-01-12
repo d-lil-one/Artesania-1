49581d5e70bad710e6072c3b6c769a953cf73dce
d-lil-one <coolignition@hotmail.com>
2014-01-12 14:27:29 +0530

    Create about_us.php

---
+++ about_us.php
@@ -0,0 +1,54 @@
+<?php
+	include 'header.html';
+	//include any specific css file	
+?>
+		<style>
+			#content {
+				margin-left:100px;
+			}
+			#content img {
+				float:left;
+				padding-left:60px;
+			}
+			#box{
+				float:left;
+				margin-left:40px;
+				height:400px;
+				width:600px;
+				padding:10px;
+				font-size:15px;
+				text-align:justify;
+				color:rgb(253, 114, 114);
+			}
+			#box p{
+				padding-left:10px;
+				font-size:15px;
+				color:#000;
+				font-family:cursive;
+			}
+			#box h2{
+				text-align:center;
+			}
+		</style>
+		</head>
+	<?php
+		include "header2.php";
+		include "profileLoginHeader.php";
+	?>
+	
+		<div id = "content">
+			<img src="images/fella4.png"></img>
+				<div id="box">
+					<h2>ABOUT ARTESANIA</h2>
+					<p>Artesania is an online community showcasing various forms of user-made artwork. It was first launched on October 9, 2013 by Neha Sharma and Simral Chaudhary. Artesania aims to provide a platform for any artist to exhibit and discuss works. Works are organized in a comprehensive category structure, including photography, painting, fashion designing, literature, Animation, filmmaking and others, along with extensive downloadable resources such as stock photography.</br> The site consists of over 100 images. In addition, Artesania users submit "favourites" and  comments daily.  Members of deviantArt may leave comments and critiques on individual art pages, allowing the site to be called a [free] peer evaluation application.Individual arts are displayed on their own pages, with a list of statistical information about the image, as well as place for uploader and member comments. These art pieces are required to be organized into categories when a member uploads an image, and this facilitates the Artesanias search engine in finding images concerning similar topics.</br> Individual members can also organize their own art pieces into folders, allowing further organization on their personal pages. </br>These personal member pages, or profiles, show a members personally uploaded art pieces postings. Also displayed are a members favourites, or collection of other users images from Artesania, which the member can save to a folder of its own. Other opportunities for communication among members are artesanias forums and chatrooms; chatrooms being for group instant messaging, and forums being for more structured, long-term discussions.</p>
+				</div>
+		</div>
+		<div class="clear"></div>
+	</div><!--div main close-->
+	<?php 
+		include "footer.php";
+	?>
+</div><!--div container close-->
+</body>
+</html>
+		
